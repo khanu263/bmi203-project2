f880dae7a364980bd8e5f6062b251675e1a2e1a5
Umair Khan <umair.khan@ucsf.edu>
2022-01-18 19:15:46 -0800

    Implement unit tests

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,27 +1,43 @@
-# write tests for bfs
-import pytest
+import networkx as nx
 from search import graph
 
-@pytest.fixture
 def test_bfs_traversal():
-    """
-    TODO: Write your unit test for a breadth-first
-    traversal here. Create an instance of your Graph class 
-    using the 'tiny_network.adjlist' file and assert 
-    that all nodes are being traversed (ie. returns 
-    the right number of nodes, in the right order, etc.)
-    """
-    pass
+
+    # Create graph and make sure you can't start at a nonexistent node
+    g = graph.Graph("data/tiny_network.adjlist")
+    assert g.bfs("0") == None
+
+    # For each starting point, compare my traversal implementation to
+    # the BFS traversal created using networkx. The code for using
+    # networkx for BFS traversal is derived from:
+    #
+    # https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.traversal.breadth_first_search.bfs_edges.html
+    #
+    for node in g.graph:
+        traversal = g.bfs(node)
+        nx_traversal = [node] + [v for u, v in nx.bfs_edges(g.graph, node)]
+        assert traversal == nx_traversal
 
 def test_bfs():
-    """
-    TODO: Write your unit test for your breadth-first 
-    search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
-    a (shortest) path between them.
-    
-    Include an additional test for nodes that are not connected 
-    which should return None. 
-    """
-    pass
+
+    # The first part of this test involves the following dummy graph
+    # I made in data/dummy.adjlist:
+    #
+    #  A --> B --> C
+    #        |     |
+    #        |     |         F
+    #        v     v
+    #        E <-- D
+    #
+    g = graph.Graph("data/dummy.adjlist")
+    assert len(g.bfs("A", end = "B")) == 2
+    assert len(g.bfs("A", end = "C")) == 3
+    assert len(g.bfs("A", end = "D")) == 4
+    assert len(g.bfs("A", end = "E")) == 3
+    assert g.bfs("A", end = "F") == None
+    assert g.bfs("G", end = "A") == None
+
+    # I then load in the citation network and test an unconnected node and a known path
+    g = graph.Graph("data/citation_network.adjlist")
+    assert g.bfs("34916529", end = "Nadav Ahituv") == None
+    assert len(g.bfs("33637727", end = "34827709")) == 3
